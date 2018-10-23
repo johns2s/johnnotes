273b26b394fd6d19e1d1da5539610ad932d82027
johns2s <johns2ses@zoho.com>
2018-10-23 13:37:24 -0400

    halfway to looking decent

--- index.php
+++ index.php
@@ -4,8 +4,20 @@ include("header.php");
 
 <main>
 
-  <h2>Popular Quackfeeds</h2>
+  <h2>Popular Quackstreams</h2>
   <div class = "listQuack">
+    <div class = "listQuackItem">
+      <h3 class = "cardTitle cardTitleH3">devnotes</h3>
+      <p class = "listQuackItemDescription">our founder and savior</p>
+    </div>
+    <div class = "listQuackItem">
+      <h3 class = "cardTitle cardTitleH3">devnotes</h3>
+      <p class = "listQuackItemDescription">our founder and savior</p>
+    </div>
+    <div class = "listQuackItem">
+      <h3 class = "cardTitle cardTitleH3">devnotes</h3>
+      <p class = "listQuackItemDescription">our founder and savior</p>
+    </div>
     <div class = "listQuackItem">
       <h3 class = "cardTitle cardTitleH3">devnotes</h3>
       <p class = "listQuackItemDescription">our founder and savior</p>
---
+++ user.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+?>
+
+<main class = "cols2">
+  <div class = "content card">
+    <h2 class = "cardTitle cardTitleH2">Recent Quacks</h2>
+    <div class = "userQuack">
+      <h3 class = "userQuackTitle">Welcome to my Quackstream!</h3>
+      <p class = "userQuackPreview">welcome to Quack</p>
+      <div class = "userQuackPlus"><img class = "plus" src = "plus.png"></div>
+    </div>
+  </div>
+  <div class = "sidebar card">
+    <h2 class = "cardTitle cardTitleH2">John Sarbak</h2>
+    <label class = "label">Bio</label>
+    <p>Hi, I'm the dev of Quack! Say hi!</p>
+    <label class = "label">Stats</label>
+  </div>
+</main>
+
+<?php
+include("footer.php");
+?>
