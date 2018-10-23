4f7f3fd504ca4d3529b2490b326956d27d392065
johns2s <johns2ses@zoho.com>
2018-10-22 21:38:49 -0400

    basic index skeleton

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>&copy 2018-<?php echo date("Y"); ?> Quack devs &#8226; Made with &heartsuit;</footer>
+
+</body>
+
+</html>
+
+<?php mysqli_close($conn); ?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include("header.php");
+?>
+
+<main>
+
+  <h2>Popular Quackfeeds</h2>
+  <div class = "listQuack">
+    <div class = "listQuackItem">
+      <h3 class = "listQuackItemTitle">devnotes</h3>
+      <p class = "listQuackItemDescription">our founder and savior</p>
+    </div>
+  </div>
+
+</main>
+
+<?php
+include("footer.php");
+?>
