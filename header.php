8776dc9d12fdb276d0ea6af995a63ed7a362010b
johns2s <johns2ses@zoho.com>
2018-10-28 19:19:54 -0400

    dont need js yet

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ include("config.php");
   <meta name="viewport" content="width = device-width, initial-scale = 1.0">
   <link rel="stylesheet" type="text/css" href="style.css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans|Righteous" rel="stylesheet" type="text/css">
-  <script src="https://cdn.jsdelivr.net/npm/moment@2/moment.min.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/moment@2/moment.min.js"></script> -->
   <!-- JSDelivr down? Try this: <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script> -->
 </head>
 
