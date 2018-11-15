44fd3ebd30b913004b041b5f59bd2f679f13c0cc
johns2s <johns2ses@zoho.com>
2018-11-14 22:28:36 -0500

    build edit.php

---
+++ edit.php
@@ -0,0 +1,18 @@
+<?php
+include("header.php");
+?>
+
+<div class = "tools">
+  <h3>Edit Quack</h3>
+  <a href = "#" class = "btn toolBtn">Publish</a>
+</div>
+
+<main style = "padding-top: 200px">
+  <div>
+    <h2>Edit Quack</h2>
+  </div>
+</main>
+
+<?php
+include("footer.php");
+?>
